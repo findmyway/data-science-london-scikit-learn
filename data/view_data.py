71de03bd730051c73af9e2952f77c0313052f692
TianJun <tianjun.cpp@gmail.com>
2014-09-23 22:40:12 +0800

    svm(rbf) + grid search

--- data/view_data.py
+++ data/view_data.py
@@ -2,6 +2,7 @@
 data visualization
 """
 import matplotlib.pyplot as plt
+from data import trainX  # 这里pycharm会提示出错，不用管，另外commit的时候注意不要勾选optimize import
 
 plt.boxplot(trainX)
 plt.savefig("./trainX_boxplot.png")
