88d2d712d1486bd6cbf770aecb8d4b98c436ff83
TianJun <tianjun.cpp@gmail.com>
2014-09-24 10:59:24 +0800

    remove the preprocess part, it's useless in last commit

--- svm/svm.py
+++ svm/svm.py
@@ -6,7 +6,6 @@ from pprint import pprint
 from sklearn.svm import SVC
 import numpy as np
 from sklearn.grid_search import GridSearchCV
-from sklearn.preprocessing import scale
 
 from data.data import trainX, trainY, testX
 
@@ -16,8 +15,6 @@ def naive_svm():
     try a naive svm method!
     :return: numpy, test labels
     """
-    preprocess(trainX)
-
     # grid search
     params = {'kernel': ['rbf'],
               'C': [0.5, 1, 2, 3, 4, 5],
@@ -33,16 +30,6 @@ def naive_svm():
     best_clf.fit(trainX, trainY)
     y = best_clf.predict(testX)
     return y
-
-
-def preprocess(x):
-    """
-    do some preprocessing on raw data
-    :param x: numpy array
-    :return:
-    """
-    # scale to standard distribution
-    scale(x)
 
 
 if __name__ == '__main__':
